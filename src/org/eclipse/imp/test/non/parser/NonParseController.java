2f676aa360f5b14672d31b182182a9e9c980d915
Robert M. Fuhrer <rmfuhrer@optonline.net>
2009-03-30 19:15:57 +0000

    Pulled getParser() and getLexer() up to SimpleLPGParseController, now that IParser and ILexer have enough API to use them in lieu of language-specific parser/lexer classes. Removed unnecessary override of initialize(), which just called super.initialize(). Accommodate change to IParseController.parse(): no longer takes a dummy "scanOnly" arg. Accommodate rename of IParseController.getNodeLocator() to getSourcePositionLocator().

--- src/org/eclipse/imp/test/non/parser/NonParseController.java
+++ src/org/eclipse/imp/test/non/parser/NonParseController.java
@@ -1,52 +1,21 @@
 package org.eclipse.imp.test.non.parser;
 
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.imp.services.ILanguageSyntaxProperties;
-import org.eclipse.imp.model.ISourceProject;
-import org.eclipse.imp.parser.ILexer;
-import org.eclipse.imp.parser.IMessageHandler;
 import org.eclipse.imp.parser.IParseController;
-import org.eclipse.imp.parser.IParser;
 import org.eclipse.imp.parser.ISourcePositionLocator;
 import org.eclipse.imp.parser.MessageHandlerAdapter;
 import org.eclipse.imp.parser.SimpleLPGParseController;
-
+import org.eclipse.imp.services.ILanguageSyntaxProperties;
 import org.eclipse.imp.test.non.Activator;
 
 /**
  */
 public class NonParseController extends SimpleLPGParseController implements IParseController {
-    private NonParser parser;
-
-    private NonLexer lexer;
-
     public NonParseController() {
         super(Activator.kLanguageID);
     }
 
-    /**
-     * @param filePath
-     *            Absolute path of file
-     * @param project
-     *            Project that contains the file
-     * @param handler
-     *            A message handler to receive error messages (or any others)
-     *            from the parser
-     */
-    public void initialize(IPath filePath, ISourceProject project, IMessageHandler handler) {
-        super.initialize(filePath, project, handler);
-    }
-
-    public IParser getParser() {
-        return parser;
-    }
-
-    public ILexer getLexer() {
-        return lexer;
-    }
-
-    public ISourcePositionLocator getNodeLocator() {
+    public ISourcePositionLocator getSourcePositionLocator() {
         return new NonASTNodeLocator();
     }
 
@@ -57,29 +26,29 @@ public class NonParseController extends SimpleLPGParseController implements IPar
     /**
     * setFilePath() should be called before calling this method.
     */
-    public Object parse(String contents, boolean scanOnly, IProgressMonitor monitor) {
+    public Object parse(String contents, IProgressMonitor monitor) {
         PMMonitor my_monitor= new PMMonitor(monitor);
         char[] contentsArray= contents.toCharArray();
 
-        if (lexer == null) {
-            lexer= new NonLexer();
+        if (fLexer == null) {
+            fLexer= new NonLexer();
         }
-        lexer.reset(contentsArray, fFilePath.toPortableString());
+        fLexer.reset(contentsArray, fFilePath.toPortableString());
 
-        if (parser == null) {
-            parser= new NonParser(lexer.getLexStream());
+        if (fParser == null) {
+            fParser= new NonParser(fLexer.getILexStream());
         }
-        parser.reset(lexer.getLexStream());
-        parser.getParseStream().setMessageHandler(new MessageHandlerAdapter(handler));
+        fParser.reset(fLexer.getILexStream());
+        fParser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
 
-        lexer.lexer(my_monitor, parser.getParseStream()); // Lex the stream to
+        fLexer.lexer(my_monitor, fParser.getIPrsStream()); // Lex the stream to
         // produce the token
         // stream
         if (my_monitor.isCancelled())
             return fCurrentAst; // TODO fCurrentAst might (probably will) be
         // inconsistent wrt the lex stream now
 
-        fCurrentAst= parser.parser(my_monitor, 0);
+        fCurrentAst= fParser.parser(my_monitor, 0);
 
         cacheKeywordsOnce();
 
