8e8479caebe7d83bc2ec0d96300260cd34a602a4
Robert M. Fuhrer <rmfuhrer@optonline.net>
2010-03-12 18:48:23 +0000

    Removed implementation of parse(), now that the basic parsing activity is taken care of by the base class implementation.

--- src/org/eclipse/imp/test/non/parser/NonParseController.java
+++ src/org/eclipse/imp/test/non/parser/NonParseController.java
@@ -1,9 +1,7 @@
 package org.eclipse.imp.test.non.parser;
 
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.imp.parser.IParseController;
 import org.eclipse.imp.parser.ISourcePositionLocator;
-import org.eclipse.imp.parser.MessageHandlerAdapter;
 import org.eclipse.imp.parser.SimpleLPGParseController;
 import org.eclipse.imp.services.ILanguageSyntaxProperties;
 import org.eclipse.imp.test.non.Activator;
@@ -13,6 +11,8 @@ import org.eclipse.imp.test.non.Activator;
 public class NonParseController extends SimpleLPGParseController implements IParseController {
     public NonParseController() {
         super(Activator.kLanguageID);
+        fLexer= new NonLexer();
+        fParser= new NonParser();
     }
 
     public ISourcePositionLocator getSourcePositionLocator() {
@@ -21,38 +21,5 @@ public class NonParseController extends SimpleLPGParseController implements IPar
 
     public ILanguageSyntaxProperties getSyntaxProperties() {
         return null;
-    }
-
-    /**
-    * setFilePath() should be called before calling this method.
-    */
-    public Object parse(String contents, IProgressMonitor monitor) {
-        PMMonitor my_monitor= new PMMonitor(monitor);
-        char[] contentsArray= contents.toCharArray();
-
-        if (fLexer == null) {
-            fLexer= new NonLexer();
-        }
-        fLexer.reset(contentsArray, fFilePath.toPortableString());
-
-        if (fParser == null) {
-            fParser= new NonParser(fLexer.getILexStream());
-        }
-        fParser.reset(fLexer.getILexStream());
-        fParser.getIPrsStream().setMessageHandler(new MessageHandlerAdapter(handler));
-
-        fLexer.lexer(my_monitor, fParser.getIPrsStream()); // Lex the stream to
-        // produce the token
-        // stream
-        if (my_monitor.isCancelled())
-            return fCurrentAst; // TODO fCurrentAst might (probably will) be
-        // inconsistent wrt the lex stream now
-
-        fCurrentAst= fParser.parser(my_monitor, 0);
-
-        cacheKeywordsOnce();
-
-        Object result= fCurrentAst;
-        return result;
     }
 }
